06ec97bed593739bfa0a3e68979cc7ef2aae5f08
kyechan99 <kyechan99@naver.com>
2017-09-17 13:21:50 +0900

    load

--- Assets/Script/MapLoad.cs
+++ Assets/Script/MapLoad.cs
@@ -5,39 +5,39 @@ using UnityEngine;
 
 public class MapLoad : MonoBehaviour
 {
-    void Start()
-    {
-        Debug.Log(readStringFromFile("MapData/map0"));
-    }
+    //void Start()
+    //{
+    //    //Debug.Log(readStringFromFile("MapData/map0"));
+    //}
 
-    string readStringFromFile(string fileName)
-    {
-        TextAsset textAsset = Resources.Load(fileName) as TextAsset;
-        string str = textAsset.text;
+    //string readStringFromFile(string fileName)
+    //{
+    //    TextAsset textAsset = Resources.Load(fileName) as TextAsset;
+    //    string str = textAsset.text;
 
-        char sp = ',';
-        string[] spString = str.Split(sp);
+    //    char sp = ',';
+    //    string[] spString = str.Split(sp);
 
-        for (int i = 0; i < spString.Length; i++)
-        {
-            Debug.Log(i + " " + spString[i]);
-        }
-        return str;
-    }
-    string pathForDocuments(string fileName)
-    {
-        if (Application.platform == RuntimePlatform.Android)
-        {
-            string path = Application.persistentDataPath;
-            path = "jar:file://" + Application.dataPath + "!/assets/";
-            //path = path.Substring(0, path.LastIndexOf('/'));
-            return Path.Combine(path, fileName);
-        }
-        else
-        {
-            string path = Application.dataPath;
-            path = path.Substring(0, path.LastIndexOf('/'));
-            return Path.Combine(path, fileName);
-        }
-    }
+    //    for (int i = 0; i < spString.Length; i++)
+    //    {
+    //        Debug.Log(i + " " + spString[i]);
+    //    }
+    //    return str;
+    //}
+    //string pathForDocuments(string fileName)
+    //{
+    //    if (Application.platform == RuntimePlatform.Android)
+    //    {
+    //        string path = Application.persistentDataPath;
+    //        path = "jar:file://" + Application.dataPath + "!/assets/";
+    //        //path = path.Substring(0, path.LastIndexOf('/'));
+    //        return Path.Combine(path, fileName);
+    //    }
+    //    else
+    //    {
+    //        string path = Application.dataPath;
+    //        path = path.Substring(0, path.LastIndexOf('/'));
+    //        return Path.Combine(path, fileName);
+    //    }
+    //}
 }
--- Assets/Script/MapUpLoad.cs
+++ Assets/Script/MapUpLoad.cs
@@ -23,16 +23,94 @@ public class MapUpLoad : MonoBehaviour
     StreamWriter swNote = null;
     float zOrder = 0;
 
+    string mapName = "0";
+
     void Start()
     {
         StringBuilder sb = new StringBuilder();
         sb.Append("0, ");
         sbSB.Add(sb);
 
-        path = pathForDocuments("Assets/Resources/MapData/map0.txt");
+    }
+
+    public void load()
+    {
+        path = pathForDocuments("Assets/Resources/MapData/map" + mapName + ".txt");
         file = new FileStream(path, FileMode.Create, FileAccess.Write);
         swNote = new StreamWriter(file);
+        //Debug.Log(readStringFromFile("MapData/map" + mapName));
+
+       // StartCoroutine("bringWaiting");
     }
+
+    public void chgMapName(UnityEngine.UI.Text txt)
+    {
+        mapName = txt.text;
+    }
+
+    //string readStringFromFile(string fileName)
+    //{
+    //    TextAsset textAsset = Resources.Load(fileName) as TextAsset;
+    //    string str = textAsset.text;
+
+    //    string[] spString = str.Split('\n');
+
+    //    for (int i = 0; i < spString.Length; i++)
+    //    {
+    //        sbSB[i].Append(spString[i]);
+            
+    //        string[] code = spString[i].Split(',');
+    //        for (int j = 0; j < code.Length; j++)
+    //        {
+    //            if (!code[j].Equals("0"))
+    //            {
+    //                GameObject obj = Instantiate(block) as GameObject;
+    //                obj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprite/Tile/Harlem/" + blockCode.ToString());
+    //                obj.transform.parent = transform;
+    //                obj.transform.localPosition = new Vector2(3 * j, 3 * i);
+    //                obj.transform.localScale = Vector2.one;
+    //            }
+    //        }
+    //    }
+    //    return str;
+    //}
+
+    IEnumerator bringWaiting()
+    {
+        while (swNote.Equals(null) || file.Equals(null))
+            yield return null;
+
+        TextAsset textAsset = Resources.Load("MapData/map" + mapName) as TextAsset;
+        string str = textAsset.text;
+
+        string[] spString = str.Split('\n');
+
+        for (int i = 0; i < spString.Length; i++)
+        {
+            if (sbSB.Count > i)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("");
+                sbSB.Add(sb);
+            }
+
+            sbSB[i].Append(spString[i]);
+
+            string[] code = spString[i].Split(',');
+            for (int j = 0; j < code.Length; j++)
+            {
+                if (!code[j].Equals("0"))
+                {
+                    GameObject obj = Instantiate(block) as GameObject;
+                    obj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprite/Tile/Harlem/" + blockCode.ToString());
+                    obj.transform.parent = transform;
+                    obj.transform.localPosition = new Vector2(3 * j, 3 * i);
+                    obj.transform.localScale = Vector2.one;
+                }
+            }
+        }
+    }
+
 
     void Update()
     {
@@ -71,10 +149,25 @@ public class MapUpLoad : MonoBehaviour
             Debug.Log(sb);
 
             GameObject obj = Instantiate(block, target.transform.position, Quaternion.identity) as GameObject;
-            obj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprite/" + blockCode.ToString() );
-            Debug.Log(blockCode.ToString());
+            obj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprite/Tile/Harlem/" + blockCode.ToString() );
+
             zOrder += 0.001f;
             obj.transform.position -= new Vector3(0,0,zOrder);
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            sbSB[j].Replace(sbSB[j][3 * i], '0', 3 * i, 1);
+
+            StringBuilder sb = new StringBuilder();
+            for (int k = 0; k < sbSB.Count; k++)
+                sb.Append(sbSB[k] + "\n");
+            Debug.Log(sb);
+
+            GameObject obj = Instantiate(block, target.transform.position, Quaternion.identity) as GameObject;
+            obj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprite/Tile/0");
+
+            zOrder += 0.001f;
+            obj.transform.position -= new Vector3(0, 0, zOrder);
         }
         else if (Input.GetKeyDown(KeyCode.Escape))
         {
